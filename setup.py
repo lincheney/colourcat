29796b24ec3064d35d6500b0739785b60baa7139
Cheney Lin <lincheney@gmail.com>
2014-12-13 17:01:11 +1100

    basic cat replication

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name = 'colourcat',
+    version = '0.1',
+    description = 'Dump files with syntax-highlighting to console, and other console utilities.',
+    url = 'https://github.com/lincheney/colourcat',
+    author = 'Cheney Lin',
+    author_email = 'lincheney@gmail.com',
+    packages = ['colourcat'],
+    scripts = ['bin/colourcat'],
+    install_requires = ['pygments'],
+)
